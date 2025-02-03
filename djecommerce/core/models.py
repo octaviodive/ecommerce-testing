c7f67b4b9a8223a50d9dade8fdc0604fed3e9062
Octavio Diaz <octaviodivework@gmail.com>
2025-02-03 08:15:59 -0400

    feat: Add Item model with essential fields and methods
    - Added Item model with the following fields: title, price, discount_price, category, label, slug, description, image
    - Implemented string representation (__str__) method
    - Added methods to generate URLs for absolute view, adding to cart, and removing from cart
    - Ensured slug is auto-generated if not provided

--- djecommerce/core/models.py
+++ djecommerce/core/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
+from django.core.validators import MinValueValidator
+from django.utils.text import slugify
+from .choices import CategoryChoices, LabelChoices, enum_to_choices
 
 # Create your models here.
+
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
 
 
 class UserProfile(models.Model):
@@ -12,3 +22,35 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    discount_price = models.FloatField(blank=True, null=True, validators=[MinValueValidator(0.0)])
+    category = models.CharField(choices=enum_to_choices(CategoryChoices), max_length=2)
+    label = models.CharField(choices=enum_to_choices(LabelChoices), max_length=1)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField()
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={'slug': self.slug})
+    
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+                       
--- djecommerce/djecommerce/settings.py
+++ djecommerce/djecommerce/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
+
 from pathlib import Path
+
+#ADDED IMPORTS
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#ADDED CONFIGURATION
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
