9cf64f50e2d85cf057c044ac23c88eddfd1a47d8
Octavio Diaz <octaviodivework@gmail.com>
2025-02-09 17:20:49 -0400

    fix some bugs

--- djecommerce/djecommerce/settings.py
+++ djecommerce/djecommerce/settings.py
@@ -131,9 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #ADDED CONFIGURATION
-
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 
 
 
