d74a8766a30851aadc64c11c86c6fbe8a392bb84
Octavio Diaz <octaviodivework@gmail.com>
2025-02-13 21:14:28 -0400

    add remove single item from car

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -5,6 +5,7 @@ from .views import (home,
                     register_user,
                     add_to_cart,
                     remove_from_cart,
+                    remove_single_item_from_cart,
                     OrderSummaryView,
                      ItemDetailView) 
 
@@ -19,5 +20,6 @@ urlpatterns = [
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('order-summary/', OrderSummaryView.as_view(), name='order_summary'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-car'),
 
 ]
--- djecommerce/core/views.py
+++ djecommerce/core/views.py
@@ -131,3 +131,34 @@ def remove_from_cart(request, slug):
     else:
         messages.info(request, "You do not have an active order")
         return redirect("core:item_detail", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:order_summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("core:item_detail", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:item_detail", slug=slug)
